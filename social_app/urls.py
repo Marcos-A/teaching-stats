ab15c06682dfa8e83a0c7bcf4fd5ac86cda1df43
Marcos-A <marcosalcocer@gmail.com>
2021-02-09 13:24:05 +0100

    Added link to the homepage from the logo at the recorded response template.

--- social_app/urls.py
+++ social_app/urls.py
@@ -5,6 +5,5 @@ from django.views.generic import TemplateView
 app_name = 'social_app'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="social_app/index.html")),
-    # path('test_login/', views.test_login, name='test_login'),
+    path('', TemplateView.as_view(template_name="social_app/index.html"), name='homepage'),
 ]
